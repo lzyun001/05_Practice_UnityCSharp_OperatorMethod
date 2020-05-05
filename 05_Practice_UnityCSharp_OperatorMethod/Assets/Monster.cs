d7d0e2b31634705b951e475ef2206ba66ab7dc3f
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-05-05 10:26:40 +0800

    上傳網頁版

---
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/Monster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [Header("血量")]
+    public int hp;
+    [Header("攻擊力")]
+    public int att;
+    [Header("治愈量")]
+    public int heal;
+}
+
---
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/Monster_Method.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Monster_Method : MonoBehaviour
+{
+    public Monster monster1;
+    public Monster monster2;
+
+    [Header("文字：輸出結果")]
+    public Text result;
+
+    public void BatAttack()
+    {
+        monster2.hp = monster2.hp - monster1.att;
+        result.text = monster2.name + " - 受到傷害：" + monster1.att + "\n" + monster2.name + " - 血量剩下：" + monster2.hp;
+    }
+
+    public void BatHeal()
+    {
+        monster1.hp = monster1.hp + monster1.heal;
+        result.text = monster1.name + " - 收到治愈：" + monster1.heal + "\n" + monster1.name + " - 血量剩下：" + monster1.hp;
+    }
+
+    public void SlimeAtt()
+    {
+        monster1.hp = monster1.hp - monster2.att;
+        result.text = monster1.name + " - 受到傷害：" + monster2.att + "\n" + monster1.name + " - 血量剩下：" + monster1.hp;
+    }
+
+    public void SlimeHeal()
+    {
+        monster2.hp = monster2.hp + monster2.heal;
+        result.text = monster2.name + " - 收到治愈：" + monster2.heal + "\n" + monster2.name + " - 血量剩下：" + monster2.hp;
+    }
+}
